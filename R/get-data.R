2ac0fcc382f435ae57ef0c4adab45361827f36be
Connor Duffin <connor.p.duffin@gmail.com>
2024-04-25 11:18:16 +0100

    New player hero pool DL functionality

--- R/get-data.R
+++ R/get-data.R
@@ -1,10 +1,15 @@
 library(purrr)
 library(rjson)
 
-get_player_data <- function(account_id, output_file) {
-    dat <- rjson::fromJSON(
-        file = gsub("acc_id", account_id,
-            "https://api.opendota.com/api/players/acc_id/matches"))
+# TODO: move base_url abstraction outside of functions
+get_player_matches <- function(account_id, output_file, recent = FALSE) {
+    if (recent) {
+        base_url <- "https://api.opendota.com/api/players/acc_id/recentMatches"
+    } else {
+        base_url <- "https://api.opendota.com/api/players/acc_id/matches"
+    }
+
+    dat <- rjson::fromJSON(file = gsub("acc_id", account_id, base_url))
     dat_cleaned <- purrr::map_depth(dat, 2, ~ifelse(is.null(.x), NA, .x))
     dat_df <- do.call(rbind, lapply(dat_cleaned, data.frame))
 
@@ -12,7 +17,15 @@ get_player_data <- function(account_id, output_file) {
     dat_df$radiant <- ifelse(dat_df$player_slot < 5, TRUE, FALSE)
     dat_df$win <- with(dat_df, !xor(radiant, radiant_win))
 
-    write.csv(dat_df, file = output_file)
+    write.csv(dat_df, file = output_file, row.names = FALSE)
+}
+
+get_player_heroes <- function(account_id, output_file) {
+    base_url <- "https://api.opendota.com/api/players/acc_id/heroes"
+    dat <- rjson::fromJSON(file = gsub("acc_id", account_id, base_url))
+    dat_df <- do.call(rbind.data.frame, dat)
+    dat_df$date <- Sys.Date()
+    write.csv(dat_df, file = output_file, row.names = FALSE)
 }
 
 get_mmr_data <- function(output_file) {
@@ -21,3 +34,5 @@ get_mmr_data <- function(output_file) {
     dat_df$date <- Sys.Date()
     write.csv(dat_df, file = output_file, row.names = FALSE)
 }
+
+
--- tests/testthat/test-get-data.R
+++ tests/testthat/test-get-data.R
@@ -1,6 +1,7 @@
-test_that("get_player_data reads in data from OpenDotA", {
+# TODO: check how to manage output file when unit testing
+test_that("get_player_data reads in matches from OpenDotA", {
     output_file <- "tmp.csv"
-    get_player_data(86745912, output_file)
+    get_player_matches(86745912, output_file)
 
     out <- read.csv(output_file)
     expect_in(
@@ -12,9 +13,38 @@ test_that("get_player_data reads in data from OpenDotA", {
           "duration",
           "start_time"),
         names(out))
-    expect_gt(nrow(out), 0)
+    expect_gt(nrow(out), 20)
     expect_true(sum(out$win) / nrow(out) >= 0.5)
     file.remove(output_file)
+})
+
+test_that("get_player_data reads in recent matches from OpenDotA", {
+    output_file <- "tmp.csv"
+    get_player_matches(86745912, output_file, recent = TRUE)
+    out <- read.csv(output_file)
+
+    expect_in(
+        c("account_id",
+          "radiant",
+          "win",
+          "match_id",
+          "hero_id",
+          "duration",
+          "start_time"),
+        names(out))
+    expect_equal(nrow(out), 20)
+    expect_true(sum(out$win) / nrow(out) <= 1.)
+    file.remove(output_file)
+})
+
+test_that("get_player_heroes reads in a players hero pool", {
+    output_file <- "tmp.csv"
+    get_player_heroes(86745912, output_file)
+    out <- read.csv(output_file)
+
+    expect_in(c("hero_id", "last_played", "games", "win", "with_games",
+                "with_win", "against_games", "against_win", "date"),
+              names(out))
 })
 
 test_that("get_mmr_data reads in data from OpenDotA", {
